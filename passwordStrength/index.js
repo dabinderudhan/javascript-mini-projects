b65597d21f688e68a3fb46f52ddd0788e0c47a97
[dabinderudhan] <[dabinder.udhan@gmail.com]>
2022-08-22 19:28:09 +0530

    strength password updates

--- passwordStrength/index.js
+++ passwordStrength/index.js
@@ -1,8 +1,13 @@
 const password = document.querySelector(".password");
 const strengthColor = document.querySelector(".strength-fill");
 
-let strongRegex =
+const strongRegex =
   /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9])(?=.{8,})/;
+
+const lowerAlphabets = /(?=.*[a-z])/;
+const upperAlphabets = /(?=.*[A-Z])/;
+const numbers = /(?=.*[0-9])/;
+const otherCharacters = /(?=.*[^a-zA-Z0-9])/;
 
 password.addEventListener("keyup", (e) => {
   const value = e.target.value;
@@ -14,44 +19,60 @@ password.addEventListener("keyup", (e) => {
     let width = 0;
     let id = setInterval(frame, 10);
     function frame() {
+      if (value.length < 5) {
+        strengthColor.style.width = 0;
+        strengthColor.style.backgroundColor = "";
+      }
+
       if (
-        (/(?=.*[a-z])/.test(value) && value.length >= 5) ||
-        /(?=.*[a-z])(?=.*[A-Z])/.test(value)
+        (lowerAlphabets.test(value) ||
+          upperAlphabets.test(value) ||
+          numbers.test(value) ||
+          otherCharacters.test(value)) &&
+        value.length >= 5
       ) {
         width = 20;
         strengthColor.style.width = width + "%";
         strengthColor.style.backgroundColor = "red";
       }
 
-      if (/(?=.*[a-z])/.test(value) && value.length >= 8) {
+      if (
+        (lowerAlphabets.test(value) ||
+          upperAlphabets.test(value) ||
+          numbers.test(value) ||
+          otherCharacters.test(value)) &&
+        value.length >= 8
+      ) {
         width = 40;
         strengthColor.style.width = width + "%";
         strengthColor.style.backgroundColor = "orange";
       }
 
-      if (/(?=.*[a-z])(?=.*[A-Z])/.test(value) && value.length >= 8) {
+      if (
+        lowerAlphabets.test(value) &&
+        upperAlphabets.test(value) &&
+        value.length >= 8
+      ) {
         width = 60;
         strengthColor.style.width = width + "%";
         strengthColor.style.backgroundColor = "orange";
       }
 
       if (
-        /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/.test(value) &&
-        value.length >= 8
+        /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/.test(value) ||
+        /(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?=.{8,})/.test(value)
       ) {
         width = 80;
         strengthColor.style.width = width + "%";
         strengthColor.style.backgroundColor = "orange";
       }
 
-      if (
-        /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9])/.test(value) &&
-        value.length >= 8
-      ) {
+      if (strongRegex.test(value)) {
         width = 100;
         strengthColor.style.width = width + "%";
         strengthColor.style.backgroundColor = "green";
       }
+
       clearInterval(id);
     }
   }
