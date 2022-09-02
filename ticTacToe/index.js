f917e94bb64e3e1036b54395e289cc3a9177c633
dabinder.udhan@gmail.com <dabinder.udhan@gmail.com>
2022-09-02 10:21:32 +0530

    third commit

--- ticTacToe/index.js
+++ ticTacToe/index.js
@@ -1,9 +1,17 @@
 const boxes = document.querySelectorAll(".col");
+const resetBtn = document.querySelector(".reset-btn");
+const showResult = document.querySelector(".result");
+
+let right = false;
 
 boxes.forEach((box) => {
-  box.addEventListener("click", (e) => {
-    const id = e.target.dataset.id;
-    if (id % 2) box.innerHTML = "x";
+  box.addEventListener("click", () => {
+    right = !right;
+    if (right) box.innerHTML = "x";
     else box.innerHTML = "o";
   });
+});
+
+resetBtn.addEventListener("click", () => {
+  boxes.forEach((box) => (box.innerHTML = ""));
 });
