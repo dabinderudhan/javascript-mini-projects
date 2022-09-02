1238add4429b5b9a194da6247d448d0a7ea68858
dabinder.udhan@gmail.com <dabinder.udhan@gmail.com>
2022-09-02 19:28:17 +0530

    final chips input

--- chipsInput/index.js
+++ chipsInput/index.js
@@ -3,19 +3,28 @@ const showChips = document.querySelector(".show-chips");
 
 let chipArray = [];
 
+//~% Helper function
+function printChip(chip) {
+  return `<p class="chip">${chip} <span>&times</span></p>`;
+}
+
+//~> eventlistener to display print in the DOM
 inputChips.addEventListener("keypress", (e) => {
   const chip = e.target.value;
 
   showChips.innerHTML = `${chipArray
-    .map((chip) => `<p class="chip">${chip} <span>&times</span></p>`)
-    .join("")}<p class="chip">${chip} <span>&times</span></p>`;
+    .map((chip) => printChip(chip))
+    .join("")}${printChip(chip)}`;
 
   if (e.key === "Enter") {
     chipArray.push(chip);
     inputChips.value = "";
-    // console.log(chipArray);
   }
+});
 
-  //   const deleteBtn = showChips.querySelector(".chip span");
-  //   console.log(deleteBtn);
+//~> eventlistener to delete the chip from the array and DOM
+showChips.addEventListener("click", (e) => {
+  const chipValue = e.target.parentElement.innerText.slice(0, -2).toLowerCase();
+  chipArray = chipArray.filter((chip) => chip !== chipValue);
+  showChips.innerHTML = chipArray.map((chip) => printChip(chip)).join("");
 });
